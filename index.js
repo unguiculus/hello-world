7893a1fc541b9dc8b1c9f22eafaa65ec4fd176ec
Reinhard Nägele <unguiculus@gmail.com>
2019-03-30 21:19:36 +0100

    Print Helm instead of World

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var os = require('os');
 var app = express();
 
 app.get('/', function(req, res) {
-    res.send('Hello World from ' + os.hostname + '\n');
+    res.send('Hello Helm from ' + os.hostname + '\n');
 });
 
 app.get('/health', function(req, res) {
@@ -11,5 +11,5 @@ app.get('/health', function(req, res) {
 });
 
 app.listen(8081, function() {
-    console.log('Hello World app listening on port 8081!');
+    console.log('Hello Helm app listening on port 8081!');
 });
